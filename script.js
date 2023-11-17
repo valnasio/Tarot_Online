1e3ebab5b96d402ba0bc130c3c78ad4832d7f1c7
Rafael ValnásIo <127369635+valnasio@users.noreply.github.com>
2023-11-17 20:22:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ function createCard(image) {
     card.appendChild(back);
 
     card.addEventListener('click', () => flipCard(card));
+    updateCardImages();//alteração
 
     return card;
 }
@@ -31,7 +32,7 @@ function shuffle(array) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
-    updateCardImages();//alteração
+ 
 }
 
 function shuffleCards() {
